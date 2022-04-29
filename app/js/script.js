e84e472752a44f71240ea5d4416aa7307ec29a24
Carlos Jiménez <72168739+CJimenezDev@users.noreply.github.com>
2022-04-29 18:44:39 -0500

    Delete app directory

--- app/js/script.js
+++
@@ -1,5 +0,0 @@
-const icon_menu = document.querySelector('.icon-menu'),
-    menu = document.querySelector('.nav');
-icon_menu.addEventListener('click', function() {
-    menu.classList.toggle('flex');
-})
